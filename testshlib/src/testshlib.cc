3170263d36b4e3f22d038e1bfbf20b9bc302021d
Damien Leroux <damien.leroux@gmail.com>
2012-12-01 02:40:41 +0100

    Testing vectors.

--- testshlib/src/testshlib.cc
+++ testshlib/src/testshlib.cc
@@ -9,8 +9,16 @@ bool isTrue(bool x) {
     return !!x;
 }
 
+std::vector<int> hazcheez() {
+    std::vector<int> ret;
+    ret.push_back(23);
+    ret.push_back(42);
+    return ret;
+}
+
 MODULE(testshlib)
     .reg(mul).auto_glue()
     .reg(isTrue).auto_glue()
+    .reg(hazcheez).auto_glue()
     ;
 
