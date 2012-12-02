5c3c59cc741945a7757f5e51cfa6ff6b977c26d1
Damien Leroux <damien.leroux@gmail.com>
2012-12-02 23:33:28 +0100

    Completed implementation of std::vector<char*|const char*|std::string> traits.

--- src/Value.h
+++ src/Value.h
@@ -88,6 +88,18 @@ struct Value<char*> {
 };
 
 
+template <>
+struct Value<std::string> {
+    typedef std::string CType;
+    static CType coerceToC(SEXP v) {
+        return std::string(CHAR(STRING_ELT(v, 0)));
+    }
+    static SEXP coerceToR(CType v) {
+        return mkString(v.c_str());
+    }
+};
+
+
 template <typename T>
 SEXP coerceToR(T v) { return Value<T>::coerceToR(v); }
 
@@ -159,9 +171,47 @@ template <> struct RVecTraits<float> {
     /* should be avoided and coerced into double somewhere else */
 };
 
-template <> struct RVecTraits<std::string> { enum { type=STRSXP }; };
-template <> struct RVecTraits<char*> { enum { type=STRSXP }; };
-template <> struct RVecTraits<const char*> { enum { type=STRSXP }; };
+template <> struct RVecTraits<std::string> {
+    enum { type=STRSXP };
+    typedef std::string value_type;
+    typedef SEXP RContainerType;
+    typedef std::vector<std::string> CVecType;
+    static RContainerType initRC(SEXP value) { return value; }
+    static void Rset(RContainerType rc, CVecType& cc, int i) {
+        SET_STRING_ELT(rc, i, Value<value_type>::coerceToR(cc[i]));
+    }
+    static void Cset(CVecType& cc, RContainerType rc, int i) {
+        cc.push_back(CHAR(STRING_ELT(rc, i)));
+    }
+};
+
+template <> struct RVecTraits<char*> {
+    enum { type=STRSXP };
+    typedef char* value_type;
+    typedef SEXP RContainerType;
+    typedef std::vector<char*> CVecType;
+    static RContainerType initRC(SEXP value) { return value; }
+    static void Rset(RContainerType rc, CVecType& cc, int i) {
+        SET_STRING_ELT(rc, i, Value<value_type>::coerceToR(cc[i]));
+    }
+    static void Cset(CVecType& cc, RContainerType rc, int i) {
+        cc.push_back(const_cast<char*>(CHAR(STRING_ELT(rc, i))));
+    }
+};
+
+template <> struct RVecTraits<const char*> {
+    enum { type=STRSXP };
+    typedef const char* value_type;
+    typedef SEXP RContainerType;
+    typedef std::vector<const char*> CVecType;
+    static RContainerType initRC(SEXP value) { return value; }
+    static void Rset(RContainerType rc, CVecType& cc, int i) {
+        SET_STRING_ELT(rc, i, Value<value_type>::coerceToR(cc[i]));
+    }
+    static void Cset(CVecType& cc, RContainerType rc, int i) {
+        cc.push_back(CHAR(STRING_ELT(rc, i)));
+    }
+};
 
 template <typename T>
 struct Value<std::vector<T> > {
